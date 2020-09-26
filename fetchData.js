5692b0c8faab7a8f75bbbac8d783a2a0c66f8afd
陳鎮宇 <ccking37@gmail.com>
2020-09-27 02:05:01 +0800

    change files

--- fetchData.js
+++ fetchData.js
@@ -7,6 +7,14 @@ const cors = require('cors');
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
  
 app.post('/', function(req, res){
     // res.send('Hello World!');
