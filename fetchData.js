3179a66836f38ebc86e6bd61cd34b9b3622b6fe1
陳鎮宇 <ccking37@gmail.com>
2020-09-27 00:37:55 +0800

    first commit

---
+++ fetchData.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const tts = require('./index.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+ 
+app.post('/', function(req, res){
+    // res.send('Hello World!');
+  tts.speech({
+      key: process.env.API_KEY,
+      hl: 'en-us',
+      v: 'Linda',
+      // src: 'Hello, World!',
+      src: req.body.joke,
+      r: 0,
+      c: 'mp3',
+      f: '44khz_16bit_stereo',
+      ssml: false,
+      b64: false,
+      callback: function (error, content) {
+        // https://stackoverflow.com/questions/28440369/rendering-a-base64-png-with-express
+        var audio = Buffer.from(content, 'base64');
+
+        res.writeHead(200, {
+          'Content-Type': 'audio/mp3',
+        });
+        res.end(audio); 
+      }
+  });
+});
+ 
+app.listen(8000);
